6dd12a88b4bd5840fc7bbea5b4cc6a59e051aeb1
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-27 21:55:54 +0530

    testing folder

---
+++ cspp2 assignment/m1/Assignment-1/NameRollNumber.py
@@ -0,0 +1,11 @@
+/*print("Name: , Roll Number: ")*/
+import java.util.*;
+public class NameRollNumber {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String name = s.nextline();
+		int rollno = s.nextInt();
+		System.out.println("Name: " + name, "Roll Number: "+ rollno);
+
+	}
+}
