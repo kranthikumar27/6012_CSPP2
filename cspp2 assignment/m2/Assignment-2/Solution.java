5758a62716d5e19ca7772b64e59980e6d759bb7d
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-28 19:04:39 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 assignment/m2/Assignment-2/Solution.java
+++ cspp2 assignment/m2/Assignment-2/Solution.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
  */
 public final class Solution {
     /**
-     * Constructs the object.
+     * { var_description }.
      */
     public static final int FOUR = 4;
+    /**
+     * Constructs the object.
+     */
     private Solution() {
 
     }
     /**.
-     * { function_description }
+     * { function_description }.
      *
-     * @param      args  The arguments
+     * @param      args  The arguments.
      */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -26,11 +29,11 @@ public final class Solution {
     Need to write the rootsOfQuadraticEquation function and print the output.
     */
     /**.
-     * { function_description }
+     * { function_description }.
      *
-     * @param      a     { parameter_description }
-     * @param      b     { parameter_description }
-     * @param      c     { parameter_description }
+     * @param      a     { parameter_description }.
+     * @param      b     { parameter_description }.
+     * @param      c     { parameter_description }.
      */
     static void roots(final int a, final int b, final int c) {
         double root1 = (-b + Math.sqrt((b * b) - FOUR * a * c)) / (2 * a);
