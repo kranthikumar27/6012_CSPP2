f24014ae9ae28e7f3106c83e96fd6d473fbfc0ac
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-28 18:52:47 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 assignment/m2/Assignment-2/Solution.java
+++ cspp2 assignment/m2/Assignment-2/Solution.java
@@ -3,35 +3,35 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation function and print the output.
-	*/
-	/**
-	 * { function_description }
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      b     { parameter_description }
-	 * @param      c     { parameter_description }
-	 */
-	static void rootsOfQuadraticEquation(int a, int b, int c) {
-		double root1 = (-b + Math.sqrt((b * b) - 4*a*c))/(2 * a);
-		double root2 = (-b - Math.sqrt((b * b) - 4*a*c))/(2 * a);
-		System.out.println(root1 +" "+ root2);
+    /*
+    Do not modify this main function.
+    */
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation function and print the output.
+    */
+    /**
+     * { function_description }
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+    static void rootsOfQuadraticEquation(int a, int b, int c) {
+        double root1 = (-b + Math.sqrt((b * b) - 4*a*c))/(2 * a);
+        double root2 = (-b - Math.sqrt((b * b) - 4*a*c))/(2 * a);
+        System.out.println(root1 +" "+ root2);
 
-	}
+    }
 }
