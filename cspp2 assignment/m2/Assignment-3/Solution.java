84cb11eb7bf81e95f8ab1734dc69240ebf00f393
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-28 19:31:21 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignment/m2/Assignment-3/Solution.java
+++ cspp2 assignment/m2/Assignment-3/Solution.java
@@ -4,37 +4,37 @@ import java.util.Scanner;
  */
 
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments.
-	 */
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);  
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result = power(base, exponent);
         System.out.println(result);
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      b     { parameter_description }.
-	 * @param      e     { parameter_description }.
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	static long power(final int b, final int e) {
-		if (e == 0) {
-			return 1;
-			} else {
-				return (b * power(b, e - 1));
-			}
-		}
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      b     { parameter_description }.
+     * @param      e     { parameter_description }.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static long power(final int b, final int e) {
+        if (e == 0) {
+            return 1;
+            } else {
+                return (b * power(b, e - 1));
+            }
+        }
 }
