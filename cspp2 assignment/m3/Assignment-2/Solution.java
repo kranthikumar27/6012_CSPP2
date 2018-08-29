8b53137d4f1b42f37a4bb7372b5804698f14f4cc
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-29 23:31:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignment/m3/Assignment-2/Solution.java
+++ cspp2 assignment/m3/Assignment-2/Solution.java
@@ -16,12 +16,14 @@ public class Solution {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);      
         int n = s.nextInt();
+        int value = 10;
+        int number = 7;
         int count = 0;
         for (int i = 1; i <= n; i++) {
             int remainder = 0;
-            for(int j = i; j > 0; j = j / 10) {
-                remainder = j % 10;
-                if (remainder == 7) {
+            for(int j = i; j > 0; j = j / value) {
+                remainder = j % value;
+                if (remainder == number) {
                     count += 1;
                 }
             }
