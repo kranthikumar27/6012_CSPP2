a44be35c8ecf2f7171a4aebcb6869de7ce4df5fc
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-29 23:58:14 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignment/m3/Assignment-3/Solution.java
+++ cspp2 assignment/m3/Assignment-3/Solution.java
@@ -5,20 +5,26 @@ import java.util.Scanner;
 	*/
 public final class Solution {
 	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**.
 	 * { var_description }
 	 */
 	private static int gcd = 1;
-    /**
+    /**.
      * { function_description }
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        gcd(n1,n2);
+        gcd(n1, n2);
         System.out.println(gcd(n1, n2));
     }
     /*
@@ -42,7 +48,7 @@ public final class Solution {
 			number2 = temp;
 		}
 		for (int i = 1; i <= number2; i++) {
-			if ((number1 % i ==0) && (number2 % i == 0)) {
+			if ((number1 % i == 0) && (number2 % i == 0)) {
 				gcd = i;
 			}
 		}
