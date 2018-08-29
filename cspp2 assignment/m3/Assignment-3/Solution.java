e7f46ee9962283ed6c693514ebc2a5e9f2cd16c1
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-30 00:00:27 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignment/m3/Assignment-3/Solution.java
+++ cspp2 assignment/m3/Assignment-3/Solution.java
@@ -3,20 +3,20 @@
  */
 import java.util.Scanner;
 
-/**
+/**.
  * { item_description }
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * { var_description }
-	 */
-	private static int gcd = 1;
+    }
+    /**.
+     * { var_description }
+     */
+    private static int gcd = 1;
     /**.
      * { function_description }
      *
@@ -31,30 +31,30 @@ public final class Solution {
         System.out.println(gcd(n1, n2));
     }
     /*
-	Need to write the gcd function and print the output.
-	*/
-	/**.
-	 * { function_description }
-	 *
-	 * @param      n1    The n 1
-	 * @param      n2    The n 2
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	static int gcd(final int n1, final int n2) {
-		int temp = 0;
-		int number1 = n1;
-		int number2 = n2;
-		if (number1 > number2) {
-			temp = number1;
-			number1 = number2;
-			number2 = temp;
-		}
-		for (int i = 1; i <= number2; i++) {
-			if ((number1 % i == 0) && (number2 % i == 0)) {
-				gcd = i;
-			}
-		}
-		return gcd;
-	}
+    Need to write the gcd function and print the output.
+    */
+    /**.
+     * { function_description }
+     *
+     * @param      n1    The n 1
+     * @param      n2    The n 2
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static int gcd(final int n1, final int n2) {
+        int temp = 0;
+        int number1 = n1;
+        int number2 = n2;
+        if (number1 > number2) {
+            temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+        for (int i = 1; i <= number2; i++) {
+            if ((number1 % i == 0) && (number2 % i == 0)) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
 }
