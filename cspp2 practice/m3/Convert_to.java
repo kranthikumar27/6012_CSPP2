8cec41d32187616a5d73725ba5b61cb98c578f9b
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-30 15:28:56 +0530

    while

---
+++ cspp2 practice/m3/Convert_to.java
@@ -0,0 +1,21 @@
+/**
+ * Class for converttowhileloop.
+ */
+class Converttowhileloop {
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		int i = 2;
+		/**
+		 * { item_description }
+		 */
+		while(i<=10) {
+			System.out.println(i);
+			i = i + 2;
+		}
+		System.out.println("GoodBye!");
+	}
+}
