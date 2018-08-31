dc6e6a301815eb83c99e17a6e860db043576406c
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-08-31 12:07:24 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2 assignment/m4/Assignment-2/Solution.java
+++ cspp2 assignment/m4/Assignment-2/Solution.java
@@ -10,10 +10,10 @@ public final class Solution {
      * @param      m     { parameter_description }
      * @param      n     { parameter_description }
      */
-    /**
+    /**.
      * { var_description }
      */
-    public static final Scanner sc = new Scanner(System.in);
+    public static final Scanner SC = new Scanner(System.in);
     /**
      * Reads a matrix.
      *
@@ -24,13 +24,13 @@ public final class Solution {
      */
     public static int[][] readMatrix(final int m, final int n) {
         // Scanner s = new Scanner(System.in);
-        int a[][] = new int[m][n];
+        int[][] a = new int[m][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                int val = sc.nextInt();
+                int val = SC.nextInt();
                 a[i][j] = val;
             }
-            sc.nextLine();
+            SC.nextLine();
         }
         return a;
     }
@@ -46,11 +46,11 @@ public final class Solution {
      * @param      args  The arguments.
      */
     public static void main(final String[] args) {
-        int m1 = sc.nextInt();
-        int n1 = sc.nextInt();
+        int m1 = SC.nextInt();
+        int n1 = SC.nextInt();
         int[][] matrix1 = readMatrix(m1, n1);
-        int m2 = sc.nextInt();
-        int n2 = sc.nextInt();
+        int m2 = SC.nextInt();
+        int n2 = SC.nextInt();
         int[][] matrix2 = readMatrix(m2, n2);
         int[][] res = new int[m1][n1];
         if (m1 == m2 && n1 == n2) {
@@ -62,7 +62,7 @@ public final class Solution {
             for (int i = 0; i < m1; i++) {
                 for (int j = 0; j < n1; j++) {
                     System.out.print(res[i][j]);
-                    if(j < n1 - 1){
+                    if (j < n1 - 1) {
                         System.out.print(" ");
                     }
                 }
@@ -73,3 +73,5 @@ public final class Solution {
         }
     }
 }
+
+
