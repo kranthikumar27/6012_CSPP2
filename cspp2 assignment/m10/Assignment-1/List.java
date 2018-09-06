2dfd652ef20e3d90ce56643adc226e49eb177f7a
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-09-06 11:15:19 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2 assignment/m10/Assignment-1/List.java
+++ cspp2 assignment/m10/Assignment-1/List.java
@@ -294,7 +294,7 @@ public class List {
     {
          // write the logic
     	int count = 0;
-    	for (int i = 0;i<list.length; i++) {
+    	for (int i = 0;i < size; i++) {
     		if (list[i] == item) {
     			count++;
     		}
