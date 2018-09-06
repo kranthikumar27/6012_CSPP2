22ba55d0b21f297328590a793f0725b1b6af316e
kranthi27 <kranthikumarpinku@msitprogram.net>
2018-09-06 14:48:57 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2 assignment/m10/Assignment-1/List.java
+++ cspp2 assignment/m10/Assignment-1/List.java
@@ -389,12 +389,13 @@ public class List {
                case "add":
                 if ((tokens.length) == 2){
                 String[] t = tokens[1].split(",");
-                if (t.length == 1){
+                if (t.length == 1) {
                     l.add(Integer.parseInt(tokens[1]));
                 }
-                 else {
-                    if (t.length > 1)
+                else {
+                    if (t.length > 1) {
                         l.add(Integer.parseInt(t[0]), Integer.parseInt(t[1]));
+                    }
                     }
                 }
                 break;
@@ -405,8 +406,9 @@ public class List {
                 if (tokens.length == 2) {
                 String[] t1 = tokens[1].split(",");
                 int[] temp = new int[t1.length];
-                for (int i = 0; i < temp.length; i++)
+                for (int i = 0; i < temp.length; i++) {
                     temp[i] = Integer.parseInt(t1[i]);
+                }
                 l.addAll(temp);
                 }
                 break;
